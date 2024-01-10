cbed41118d800a1821cbc7e23b2f008a059cb01f
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-09 23:45:12 -0300

    add project's swap buttons

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -4,8 +4,19 @@ import { useLanguage } from '../../context/LanguageContext'
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Project from '../../components/Project';
 import projectsData from '../../data/projectsData';
+import { useState } from 'react';
 
 const Projects = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1));
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === projectsData.length - 1 ? 0 : prevIndex + 1));
+  };
 
   const { isDarkMode } = useTheme();
   const { language } = useLanguage();
@@ -14,16 +25,16 @@ const Projects = () => {
     <div className={`projects ${isDarkMode ? '' : 'projects--light'}`}>
       <h1 className='projects__title'>{language === 'pt' ? 'Projetos' : 'Projects'}</h1>
       <div className='projects__container'>
-        <FaArrowLeft size={28} style={{ cursor: 'pointer' }} />
+        <FaArrowLeft onClick={handlePrevClick} size={28} style={{ cursor: 'pointer' }} />
         <Project
-          title={projectsData[0].title}
-          img={projectsData[0].img}
-          about={projectsData[0].about}
-          website={projectsData[0].link}
-          stack={projectsData[0].stack}
-          github={projectsData[0].github}
+          title={projectsData[currentIndex].title}
+          img={projectsData[currentIndex].img}
+          about={projectsData[currentIndex].about}
+          website={projectsData[currentIndex].link}
+          stack={projectsData[currentIndex].stack}
+          github={projectsData[currentIndex].github}
         />
-        <FaArrowRight size={28} style={{ cursor: 'pointer' }} />
+        <FaArrowRight onClick={handleNextClick} size={28} style={{ cursor: 'pointer' }} />
       </div>
     </div>
   )
