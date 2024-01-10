c61c384cf425178e88f562a236df08d237f71225
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-10 13:43:00 -0300

    finish certificates section

--- src/pages/Certificates/index.js
+++ src/pages/Certificates/index.js
@@ -1,20 +1,52 @@
 import { useLanguage } from '../../context/LanguageContext';
 import { useTheme } from '../../context/ThemeContext';
-import './Certificates.css'
+import { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react'
 import certificatesData from '../../data/certificatesData';
+import './Certificates.css'
 
 const Certificates = () => {
 
+  const [slidePerView, setSlidePerView] = useState(2);
   const { isDarkMode } = useTheme();
   const { language } = useLanguage();
 
+  useEffect(() => {
+
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSlidePerView(1)
+      }
+      else {
+        setSlidePerView(2)
+      }
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+
+  }, [])
+
   return (
     <div className={`certificates ${isDarkMode ? '' : 'certificates--light'}`}>
-      <h2 className='certificates__title'>{language === 'pt' ? 'Certificados' : 'Certificates'}</h2>
+      <h1 className='certificates__title'>{language === 'pt' ? 'Certificados' : 'Certificates'}</h1>
       <div className='certificates__container'>
-        {certificatesData.map(certificate => (
-          <img className='certificates__img' key={certificate.id} src={certificate.imagePath} alt={certificate.name} />
-        ))}
+        <Swiper
+          slidesPerView={slidePerView}
+          navigation
+          pagination
+          grabCursor
+          loop
+        >
+          {certificatesData.map((certificate) => (
+            <SwiperSlide key={certificate.id}>
+              <img className='certificates__image' src={certificate.imagePath} alt={certificate.name} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   )
