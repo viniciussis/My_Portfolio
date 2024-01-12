05707a57f395615475fdd66730e47b1cd44e3b66
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-12 12:05:20 -0300

    add new project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,5 +1,6 @@
 import Organo from '../images/projects/Organo.png'
 import VS from '../images/projects/Virtual-Space.png'
+import Clock from '../images/projects/Clock.png'
 
 const projectsData = [
   {
@@ -12,11 +13,19 @@ const projectsData = [
   },
   {
     title: 'Virtual Space',
-    about: 'Desenvolvimento de um mini blog em React, com ênfase em conceitos avançados, incluindo React Router, rotas dinâmicas, Single Page Applications (SPAs), e integração de bibliotecas como react-markdown e react-icons.',
+    about: 'Desenvolvimento de um mini blog em React, com ênfase em conceitos como React Router, rotas dinâmicas, Single Page Applications (SPAs), e integração de bibliotecas como react-markdown e react-icons.',
     demo: 'https://virtual-space-three.vercel.app',
     repository: 'https://github.com/viniciussis/Virtual_Space.git',
     stack: 'JavaScript, CSS, HTML, ReactJS',
     img: VS
+  }
+  ,{
+    title: 'My Clock',
+    about: 'Um cronômetro de tarefas em React/TypeScript, que cria card de tarefas dinâmicamente e um cronômetro para o tempo de execução dessas tarefas',
+    demo: 'https://my-clock-five.vercel.app/',
+    repository: 'https://github.com/viniciussis/My_Clock.git',
+    stack: 'TypeScript, CSS, HTML, React',
+    img: Clock
   }
 ];
 
