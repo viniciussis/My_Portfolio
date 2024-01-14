8b25f57950d0dcf4c42b7e73b0e3f03392f29c65
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-13 23:45:35 -0300

    add project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,32 +1,41 @@
 import Organo from '../images/projects/Organo.png'
 import VS from '../images/projects/Virtual-Space.png'
 import Clock from '../images/projects/Clock.png'
+import CookinUp from "../images/projects/Cookin' Up.png"
 
 const projectsData = [
   {
-    title: 'Organo',
-    about: 'Plataforma para criar e organizar dinamicamente times ou equipes, adaptando-se às necessidades específicas do usuário.',
-    demo: 'https://organo-project-jet.vercel.app/',
-    repository: 'https://github.com/viniciussis/Organo_Project.git',
-    stack: 'JavaScript, CSS, HTML, ReactJS',
-    img: Organo
+    title: "Cookin'Up",
+    about: 'Gerenciador de ingredientes e receitas em Vue',
+    demo: 'https://cookin-up-two.vercel.app',
+    repository: 'https://github.com/viniciussis/Cookin_Up',
+    stack: 'Vue, Typescript',
+    img: CookinUp
   },
   {
     title: 'Virtual Space',
     about: 'Desenvolvimento de um mini blog em React, com ênfase em conceitos como React Router, rotas dinâmicas, Single Page Applications (SPAs), e integração de bibliotecas como react-markdown e react-icons.',
     demo: 'https://virtual-space-three.vercel.app',
     repository: 'https://github.com/viniciussis/Virtual_Space.git',
-    stack: 'JavaScript, CSS, HTML, ReactJS',
+    stack: 'JavaScript, ReactJS',
     img: VS
-  }
-  ,{
+  },
+  {
     title: 'My Clock',
     about: 'Um cronômetro de tarefas em React/TypeScript, que cria card de tarefas dinâmicamente e um cronômetro para o tempo de execução dessas tarefas',
     demo: 'https://my-clock-five.vercel.app/',
     repository: 'https://github.com/viniciussis/My_Clock.git',
-    stack: 'TypeScript, CSS, HTML, React',
+    stack: 'TypeScript, React',
     img: Clock
-  }
+  },
+  {
+    title: 'Organo',
+    about: 'Plataforma para criar e organizar dinamicamente times ou equipes, adaptando-se às necessidades específicas do usuário.',
+    demo: 'https://organo-project-jet.vercel.app/',
+    repository: 'https://github.com/viniciussis/Organo_Project.git',
+    stack: 'JavaScript, ReactJS',
+    img: Organo
+  },
 ];
 
 export default projectsData
