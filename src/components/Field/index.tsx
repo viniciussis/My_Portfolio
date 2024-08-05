8c93b357231d418100f8a6905e3800fe5a7e3c85
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-08-04 21:50:14 -0300

    Remove dropdown and add textfield
    I removed an unused dropdown component and added a new type in the field component

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -3,18 +3,24 @@ import './Field.scss'
 
 interface FieldProps {
   type?: 'text' | 'number'
+  textArea?: boolean
   mandatory?: boolean
   label: string
 }
 
 const Field: React.FC<FieldProps> = ({
   mandatory = false,
+  textArea = false,
   type = 'text',
   label,
 }) => {
   return (
     <label className="field">
-      <input className="field__input" type={type} required={mandatory} />
+      {textArea ? (
+        <textarea className="field__input" maxLength={400} required />
+      ) : (
+        <input className="field__input" type={type} required={mandatory} />
+      )}
       <span className="field__label">{label}</span>
     </label>
   )
--- src/pages/Contact/EmailForm/index.tsx
+++ src/pages/Contact/EmailForm/index.tsx
@@ -5,12 +5,13 @@ import './EmailForm.scss'
 
 const EmailForm = () => {
   const { t } = useTranslation('global')
+
   return (
     <form className="emailForm">
       <Field label={t('emailForm.1')} />
       <Field label="Email" />
       <Field label={t('emailForm.2')} />
-      <Field label={t('emailForm.3')} />
+      <Field label={t('emailForm.3')} textArea={true} />
       <Button type="submit">{t('emailForm.button')}</Button>
     </form>
   )
