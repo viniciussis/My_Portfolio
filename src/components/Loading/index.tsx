8e539148a851467c394e75456d7dfc0dc59e0bf1
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-08-09 19:37:23 -0300

    Update responsivity and style

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,11 +1,13 @@
 import { LinearProgress } from '@mui/material'
 import './Loading.scss'
+import { useTranslation } from 'react-i18next'
 
 const Loading = () => {
+  const { t } = useTranslation('global')
   return (
-    <div className='loading'>
-      <LinearProgress color="secondary" />
-      <h1 className="loading__title">Carregando...</h1>
+    <div className="loading">
+      <h1 className="loading__title">{t('loading.1')}</h1>
+      <LinearProgress color="success" />
     </div>
   )
 }
