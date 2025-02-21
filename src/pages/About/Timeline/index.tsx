679b1db3a15c9792483704a26ccc0385ab86d9a5
Vinícius Silva Santos <vinicius01012@gmail.com>
2025-02-20 23:10:07 -0300

    chore: update personal bio and timeline in about me section

--- src/pages/About/Timeline/index.tsx
+++ src/pages/About/Timeline/index.tsx
@@ -10,20 +10,26 @@ const Timeline = () => {
       <h2 className="timeline__sub">{t('aboutpage.timeline.title')}</h2>
       <div className="timeline__container">
         <EventMarker
-          title={t('aboutpage.timeline.1.title')}
-          subtitle={t('aboutpage.timeline.1.sub')}
-          description={t('aboutpage.timeline.1.description')}
-        />
-        <EventMarker
-          title={t('aboutpage.timeline.2.title')}
-          subtitle={t('aboutpage.timeline.2.sub')}
-          description={t('aboutpage.timeline.2.description')}
-          position="right"
+          title={t('aboutpage.timeline.4.title')}
+          subtitle={t('aboutpage.timeline.4.sub')}
+          description={t('aboutpage.timeline.4.description')}
         />
         <EventMarker
           title={t('aboutpage.timeline.3.title')}
           subtitle={t('aboutpage.timeline.3.sub')}
           description={t('aboutpage.timeline.3.description')}
+          position="right"
+        />
+        <EventMarker
+          title={t('aboutpage.timeline.2.title')}
+          subtitle={t('aboutpage.timeline.2.sub')}
+          description={t('aboutpage.timeline.2.description')}
+        />
+        <EventMarker
+          title={t('aboutpage.timeline.1.title')}
+          subtitle={t('aboutpage.timeline.1.sub')}
+          description={t('aboutpage.timeline.1.description')}
+          position="right"
         />
       </div>
     </section>
